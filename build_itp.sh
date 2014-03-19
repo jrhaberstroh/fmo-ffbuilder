ac5d4883c05de4db4ecbc9f473d89ee3eb77f6d8
John Haberstroh <jhaberstroh@berkeley.edu>
2014-03-19 16:32:25 -0700

    build_itp has been modified for greater success

--- build_itp.sh
+++ build_itp.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-echo "BCL    Ligand" > residuetypes.dat
+# ============GENERATE A FORCEFIELD====================
+if [ -e output.ff ]; then
+    rm -r output.ff
+fi
+mkdir output.ff
 python prm2gmx.py
-cp dat/forcefield.itp output.ff/.
-pdb2gmx -ff output -f dat/bchl.gro -o output.ff/bcl.gro -p output.ff/bcl.itp
-rm residuetypes.dat
+
+
+# ============GENERATE AN ITP====================
+#echo "BCL    BCL" > residuetypes.dat
+#cp dat/forcefield.itp output.ff/.
+#pdb2gmx -ff output -f dat/bchl.gro -o output.ff/bcl.gro -p output.ff/bcl.top -i output.ff/bcl_posre.itp
+#tail -n+21 output.ff/bcl.top | head -n-8 >> output.ff/bcl.itp
+#rm residuetypes.dat
+
+
+# ============BUILD FMO TOPOLOGY====================
+if [ -e testdir ]; then
+    rm -r testdir
+fi
+mkdir testdir
+cp -r dat/amber99sb-ildn.ff testdir/ambermod.ff
+
+tail -n+30 output.ff/bcl.rtp >> testdir/ambermod.ff/aminoacids.rtp
+cat output.ff/atomtypes.atp >> testdir/ambermod.ff/atomtypes.atp
+cat output.ff/ffbonded.atp >> testdir/ambermod.ff/ffbonded.atp
+cd testdir
+pdb2gmx -f ../dat/4BCL.pdb -ff ambermod -chainsep id_and_ter -water tip3p
